26f6f89b3d608c07ba5acc5883470dd313c3116f
cyanlife <86389330+cyanlifedev@users.noreply.github.com>
2022-07-14 04:49:05 -0500

    Add files via upload

---
+++ randomthing.bat
@@ -0,0 +1,22 @@
+echo off
+cls
+echo nothing you can do so enjoy the matrix
+pause
+:matrix
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+echo %random%
+goto matrix
